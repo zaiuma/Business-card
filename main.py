a7225db485fb5e9e31d702801393e5f11cf27b5a
Your Name <you@example.com>
2025-11-30 23:51:25 +0400

    Initial commit: Business card

---
+++ main.py
@@ -0,0 +1,13 @@
+#main.py
+#Скрипт для создания "Визитной карточки"
+
+name = "Умалатова Зайнаб Дагировна"
+city = "Астрахань"
+speciality = "Специалист по Информационной безопасности"
+githup_username = "Zaiuma"
+
+print('Визитная карточка:')
+print(f'ФИ0 специалиста: {name}')
+print(f'Город: {city}')
+print(f'Профессия: {speciality}')
+print(f'GitHub-аккаунт: {githup_username}')
